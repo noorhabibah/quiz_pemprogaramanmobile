50be6f67cf178155cd3a48d20d6eb9321087a241
noorhabibah <146051944+noorhabibah@users.noreply.github.com>
2023-10-25 16:32:34 +0800

    Create type_checking.dart

---
+++ type_checking.dart
@@ -0,0 +1,23 @@
+class MyData<T>{
+  T data;
+  
+  MyData(this.data);
+}
+
+//import'mydata.dart';
+
+void check(dynamic data){
+  if (data is MyData<String>){
+    print("String");
+  } else if (data is MyData<num>){
+    print("num");
+  } else{
+    print("Object");
+  }
+}
+
+void main(){
+  check(MyData("Habibah"));
+  check(MyData(100));
+  check(MyData(true));
+}
